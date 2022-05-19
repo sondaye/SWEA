961ab188a4f5fc16f75d619e5a2ae998baa6e8ea
sondayae\sonda <sondayae@gmail.com>
2022-05-20 08:43:29 +0900

    SW Expert Academy Solution D1 Problem no. 2046

---
+++ D1/prob_2046.java
@@ -0,0 +1,14 @@
+package SW_Expert.D1;
+import java.util.Scanner;
+
+public class prob_2046 {
+
+	public static void main(String[] args) {
+		// 스탬프 찍기
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for(int i=0;i<num;i++) {
+			System.out.print("#");
+		}
+	}
+}
